efb4369d756ab9f58cbc1b664238a907b5b8d586
Jonnie Ringo <web.projects@yahoo.com>
2018-09-30 21:09:32 -0700

    Deploying to Heroku, push 2

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV === 'production') {
 
   // Create route
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirnmae, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
